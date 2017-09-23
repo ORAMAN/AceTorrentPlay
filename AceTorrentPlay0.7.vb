f09c408dc920a1e4a100fd4e49a2062cc35d7f7f
ORAMAN <oraman.na.ya@yandex.ru>
2017-09-23 09:13:32 +0900

    Update AceTorrentPlay0.7.vb

--- AceTorrentPlay0.7.vb
+++ AceTorrentPlay0.7.vb
@@ -508,7 +508,7 @@ Namespace RemoteFork.Plugins
                     .ImageLink = "http://s1.iconbird.com/ico/1112/Television/w256h25613523820647.png"
                     Try
                         If System.IO.File.Exists(System.IO.Path.GetTempPath & "MyTraf.tmp") = False Then
-                            WC.DownloadFile("http://super-pomoyka.us.to/trash/ttv-list/MyTraf.php", System.IO.Path.GetTempPath & "MyTraf.tmp")
+                            WC.DownloadFile("http://pomoyka.win/trash/ttv-list/MyTraf.php", System.IO.Path.GetTempPath & "MyTraf.tmp")
                         End If
                         .Description = "<html><img src="" http://torrent-tv.ru/images/logo.png""></html><p>" & WC.DownloadString(System.IO.Path.GetTempPath & "MyTraf.tmp")
                     Catch ex As Exception
@@ -2355,13 +2355,13 @@ Namespace RemoteFork.Plugins
             Dim PathFileUpdateTime As String = System.IO.Path.GetTempPath() & NamePlayList & ".UpdateTime.tmp"
             Dim PathFilePlayList As String = System.IO.Path.GetTempPath() & NamePlayList & ".PlayList.m3u8"
 
-            Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.CreateHttp("http://super-pomoyka.us.to/trash/ttv-list/ttv." & NamePlayList & ".iproxy.m3u?ip=" & IPAdress & ":" & PortAce)
+            Dim request As System.Net.HttpWebRequest = System.Net.HttpWebRequest.CreateHttp("http://pomoyka.win/trash/ttv-list/ttv." & NamePlayList & ".iproxy.m3u?ip=" & IPAdress & ":" & PortAce)
             request.Method = "HEAD"
             request.ContentType = "text/html"
             request.KeepAlive = True
             request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
             request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-            request.Host = "super-pomoyka.us.to"
+            request.Host = "pomoyka.win"
             Dim response As System.Net.HttpWebResponse = CType(request.GetResponse(), System.Net.HttpWebResponse)
             Dim responHeader = response.GetResponseHeader("Last-Modified")
             response.Close()
@@ -2393,9 +2393,9 @@ Namespace RemoteFork.Plugins
             WC.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
             WC.Encoding = System.Text.Encoding.UTF8
 
-            Dim PlayList As String = WC.DownloadString("http://super-pomoyka.us.to/trash/ttv-list/ttv." & NamePlayList & ".iproxy.m3u?ip=" & IPAdress & ":" & PortAce)
+            Dim PlayList As String = WC.DownloadString("http://pomoyka.win/trash/ttv-list/ttv." & NamePlayList & ".iproxy.m3u?ip=" & IPAdress & ":" & PortAce)
             System.IO.File.WriteAllText(PathFilePlayList, PlayList.Replace("(Эротика)", "(Эротика 18+)"))
-            WC.DownloadFile("http://super-pomoyka.us.to/trash/ttv-list/MyTraf.php", System.IO.Path.GetTempPath & "MyTraf.tmp")
+            WC.DownloadFile("http://pomoyka.win/trash/ttv-list/MyTraf.php", System.IO.Path.GetTempPath & "MyTraf.tmp")
             WC.Dispose()
         End Sub
 
